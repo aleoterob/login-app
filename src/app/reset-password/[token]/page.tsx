3b1e51723ab9d54e1d7f7166a8a21747ac0b5646
Alejandro Otero <aleoterob@gmail.com>
2025-01-14 15:08:40 -0300

    Updates reset password logic

--- src/app/reset-password/[token]/page.tsx
+++ src/app/reset-password/[token]/page.tsx
@@ -36,7 +36,7 @@ const ResetPasswordPage = () => {
     >
       {/* Contenido de la página */}
       <div className="relative z-10 flex justify-center items-center w-full h-full">
-        <ResetPasswordForm />
+        <ResetPasswordForm email="example@example.com" />
       </div>
     </div>
   );
