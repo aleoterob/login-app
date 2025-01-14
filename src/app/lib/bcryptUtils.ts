ef854fe1a26fddf43811658bac9ee67f7ab90a82
Alejandro Otero <aleoterob@gmail.com>
2025-01-14 16:55:43 -0300

    Updates  mail logic

--- src/app/lib/bcryptUtils.ts
+++ src/app/lib/bcryptUtils.ts
@@ -1,4 +1,5 @@
 import CryptoJS from "crypto-js";
+import bcrypt from "bcryptjs";
 
 // Clave secreta para cifrado y descifrado (debería ser algo seguro y no compartido)
 const SECRET_KEY = "mi_clave_secreta_que_deberia_ser_segura";
@@ -13,4 +14,12 @@ export const decryptToken = (encryptedToken: string): string | null => {
   const bytes = CryptoJS.AES.decrypt(encryptedToken, SECRET_KEY);
   const originalToken = bytes.toString(CryptoJS.enc.Utf8);
   return originalToken ? originalToken : null;
+};
+
+// Función para comparar contraseñas usando bcrypt
+export const comparePassword = async (
+  password: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hashedPassword);
 };
