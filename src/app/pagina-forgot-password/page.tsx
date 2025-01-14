734e75fc100bf9294144b91b05d9f5e4ab218e40
Alejandro Otero <aleoterob@gmail.com>
2025-01-14 11:44:33 -0300

    Updates forgot password page

--- src/app/pagina-forgot-password/page.tsx
+++ src/app/pagina-forgot-password/page.tsx
@@ -1,32 +1,6 @@
-"use client";
+import ForgotPasswordForm from "../components/ForgotPasswordForm";
 
-import ResetPasswordForm from "../../app/components/ResetPasswordForm";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-
-const ResetPasswordPage = () => {
-  const pathname = usePathname();
-  const [token, setToken] = useState<string | null>(null);
-  const [isValidToken, setIsValidToken] = useState(false);
-
-  useEffect(() => {
-    const pathSegments = pathname.split("/");
-    const tokenFromPath = pathSegments[pathSegments.length - 1];
-    if (tokenFromPath) {
-      setToken(tokenFromPath);
-
-      // Aquí puedes agregar la lógica para validar el token con el backend
-      // Por ahora, asumimos que el token es válido
-      setIsValidToken(true);
-    }
-
-    if (isValidToken) {
-      console.log("Token recibido:", token);
-    } else {
-      console.log("Token no válido o no proporcionado.");
-    }
-  }, [pathname]);
-
+const ForgotPasswordPage = () => {
   return (
     <div className="relative w-full h-screen">
       {/* Video de fondo */}
@@ -38,12 +12,12 @@ const ResetPasswordPage = () => {
         src="/videos/video-c.mp4" // Asegúrate de que el video esté en la carpeta public
       />
 
-      {/* Contenido de la página */}
-      <div className="relative z-10 flex flex-col justify-center items-center min-h-screen gap-8">
-        <ResetPasswordForm />
+      {/* Formulario de Olvidó Contraseña */}
+      <div className="relative z-10">
+        <ForgotPasswordForm />
       </div>
     </div>
   );
 };
 
-export default ResetPasswordPage;
+export default ForgotPasswordPage;
